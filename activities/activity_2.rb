27d66e4b19dc21cf63e5f2c3e13346b59dcea2b6
SvAcez <spectral107@gmail.com>
2022-06-20 16:41:50 +0800

    activity 2: create a script that shows fullname

--- activities/activity_2.rb
+++ activities/activity_2.rb
@@ -3,3 +3,10 @@
 #  Values will be your respective names.
 #  Write a function that takes a Hash and returns the full name.
 #  Print on console the result.
+def full_name (name_value)
+  puts "#{name_value[:firstname]}"
+  puts "#{name_value[:middle_name]}"
+  puts "#{name_value[:lastname]}"
+end
+
+p full_name ({firstname:'joshua caleb', middle_name:'dv',lastname:'cypres'})
