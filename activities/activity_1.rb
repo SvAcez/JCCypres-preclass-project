16ca19a2d2b5d89d7fc5153f8e8ba0573f08e0e8
SvAcez <spectral107@gmail.com>
2022-06-20 16:24:07 +0800

    acitivity 1: creates a script that finds the average of an adult

--- activities/activity_1.rb
+++ activities/activity_1.rb
@@ -2,3 +2,13 @@
 #  Write a function that takes an Array of ages,
 #  the function will return the average age of adults.
 #  Print on console the result.
+def calculate_age(ages)
+  sum = 0
+  ages.each do |age|
+    sum += age
+  end
+  return sum / ages.length
+
+end
+
+p calculate_age([20, 19, 23, 27, 35])
